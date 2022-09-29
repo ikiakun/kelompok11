fe58a6e9bc8217f0d5bc0d64f5f1df7903e561c2
ilhamalfa <kuro.nekogami22@gmail.com>
2022-09-29 12:11:09 +0700

    index

--- index.php
+++ index.php
@@ -9,12 +9,52 @@
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    
 
+    <div class="container">
+        <div class="row">
+            <div class="col-4 mx-auto" style="margin-top: 120px;">
+                
+                <!-- Kotak Login -->
+                <div class="card border-0 shadow">
+                    <div class="card-body">
+                        <img src="assets/img/logo-dummies.jpg" alt="" class="img img-thumbnail mt-3 rounded-circle border-0 mx-auto d-flex" style="height: 75px;">
+                        <h3 class="text-center mb-3">Sign in</h3>
+                        <form method="POST">
+                            <div class="mb-3">
+                                <label class="form-label fw-bold">Username</label>
+                                <input type="text" class="form-control" id="username" name="username" placeholder="Enter Username" autofocus>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label fw-bold">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="show" onclick="showPassword()">
+                                <label class="form-check-label text-secondary">Show Password</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100 mb-4">Sign In</button>
+                            <p class="text-secondary text-center">Don't Have An Account? <a href="" class="fw-bold link-dark">Sign up</a> </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
     <!-- Bootstrap JS -->
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Show Password -->
+    <script>
+        function showPassword(){
+            if(document.getElementById('show').checked){
+                document.getElementById('password').type = 'text';
+            }else{
+                document.getElementById('password').type = 'password';
+            }
+        }
+    </script>
 </body>
 </html>
