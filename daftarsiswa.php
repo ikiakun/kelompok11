e07db8e09f5ed448ed60d9efe18e87cd7c8aca8d
ikiakun <ikiakun@icloud.com>
2022-09-29 14:54:18 +0700

    add

---
+++ daftarsiswa.php
@@ -0,0 +1,74 @@
+<?php
+include 'config.php';
+$take = mysqli_query($db,"SELECT * FROM siswa JOIN kelas ON siswa.id_kelas = kelas.id_kelas");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Siswa</title>
+</head>
+<body>
+                        <!-- Thor - Konten mysql -->
+                         <div class="container text-center mt-4">
+                            <table class="table table-striped table-hover mt-4">
+                            <thread>
+                                <tr>
+                                <th scope="col">NIS</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Jenis Kelamin</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Kelas</th>
+                                <th scope="col">Aksi</th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                                <?php
+                                    while($data = mysqli_fetch_array($take)){                           
+                                ?>
+                                <?php
+                                    $batas = 10;
+                                    if(isset($_GET['halaman'])){
+                                        $halaman = (int)$_GET['halaman'];
+                                    }else{
+                                        $halaman = 1;
+                                    }
+                                    if($halaman > 1){
+                                        $halaman_awal = ($halaman * $batas) - $batas;
+                                    }else{
+                                        $halaman_awal = 0;
+                                    }
+                                    $data = mysqli_query($db, "SELECT * FROM siswa JOIN kelas ON siswa.id_kelas = kelas.id_kelas");
+                                    $jumlah_data = mysqli_num_rows($data);
+                                    $total_halaman = ceil($jumlah_data/$batas);
+
+                                    $result = mysqli_query($db,"SELECT * FROM siswa JOIN kelas ON siswa.id_kelas = kelas.id_kelas
+                                                                ORDER BY nis LIMIT $halaman_awal,$batas");
+                                    $no = $halaman_awal+1;
+                                    while ($data = mysqli_fetch_array($result)) {
+                                ?>
+                                <tr>
+                                    <td><?= $data['nis'] ?> </td>
+                                    <td><?= $data['nama'] ?> </td>
+                                    <td><?= $data['jenis_kelamin'] ?> </td>
+                                    <td><?= $data['alamat'] ?> </td>
+                                    <td><?= $data['nama_kelas'] ?> </td>
+                                    <td colspan="2">
+                                        <a href="editsiswa.php?id_buku=<?= $data['id_buku'] ?>" class ="btn btn-warning">edit</a>
+                                        <a href="deletesiswa.php?id_buku=<?= $data['id_buku'] ?>" class="btn btn-danger">hapus</a>
+                                    </td>
+                                </tr>
+                                <?php
+                                }
+                                ?>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </div>
+                        
+</body>
+</html>
---
+++ editbuku.php
@@ -0,0 +1,88 @@
+<?php
+include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+                        <!-- Thor - Konten mysql -->
+                        <?php
+                        $id = $_GET['id_buku'];
+                        $take = mysqli_query($db, "SELECT * FROM buku WHERE id_buku='$id'");
+                        while ($data = mysqli_fetch_assoc($take)) {
+                        ?>
+                    <div class="container m-5">
+                        <form action="" method="POST" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label class="form-label">Penulis</label>
+                            <input type="text" class="form-control" name="penulis" value="<?= $data1['penulis'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Tahun</label>
+                            <input type="text" class="form-control" name="tahun" value="<?= $data1['tahun'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Judul</label>
+                            <input type="date" class="form-control" name="judul" value="<?= $data1['judul'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Kota</label>
+                            <input type="date" class="form-control" name="kota" value="<?= $data1['kota'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Penerbit</label>
+                            <input type="date" class="form-control" name="penerbit" value="<?= $data1['penerbit'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Cover</label>
+                            <br>
+                            <img src="cover/<?= $data1['cover'] ?>" alt="" class="img-thumbnail" style="width: 50px;">  
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Ganti cover</label>
+                            <input type="file" class="form-control" name="ganticover">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Sinopsis</label>
+                            <input type="date" class="form-control" name="sinopsis" value="<?= $data1['sinopsis'] ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Stok</label>
+                            <input type="date" class="form-control" name="stok" value="<?= $data1['stok'] ?>">
+                        </div>
+                        <button type="submit" class = "btn btn-success" name="submit">Ganti!</button>
+                        </form>
+                        <?php
+                        if(isset($_POST['submit'])){
+                            $penulis = $_POST['penulis'];
+                            $tahun = $_POST['tahun'];
+                            $judul = $_POST['judul'];
+                            $kota = $_POST['kota'];
+                            $penerbit = $_POST['penerbit'];
+                            $cover = $_FILES['ganticover']['name'];
+                            $lokasi = $_FILES['ganticover']['tmp_name'];
+                            $sinopsis = $_POST['sinopsis'];
+                            $stok = $_POST['stok'];
+
+                            $query = mysqli_query ($db, 
+                            "UPDATE buku SET penulis='$penulis', tahun='$tahun', judul='$judul', kota='$kota', penerbit='$penerbit', cover='$cover', sinopsis='$sinopsis', stok='$stok'
+                            WHERE id_buku=$id");
+                            if($query){
+                            echo "<script>alert(berhasil dirubah);
+                            window.location.href='index.php'</script>";
+                            }else{
+                            echo 'Gagal Ditambahkan';
+                            }
+                            }
+                        ?>
+
+</body>
+</html>
