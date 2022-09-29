2981727fdd4b0e13989d47404adf4923a7a7372a
ikiakun <ikiakun@icloud.com>
2022-09-29 14:04:50 +0700

    add

--- daftarbuku.php
+++ daftarbuku.php
@@ -12,7 +12,7 @@ $take = mysqli_query($db,"SELECT * FROM buku");
     <title>Daftar Buku</title>
 </head>
 <body>
-    
+                        <!-- Thor - Konten mysql -->
                          <div class="container text-center mt-4">
                             <table class="table table-striped table-hover mt-4">
                             <thread>
@@ -31,8 +31,27 @@ $take = mysqli_query($db,"SELECT * FROM buku");
                             </thread>
                             <tbody>
                                 <?php
-                                while($data = mysqli_fetch_array($take)){
-                                
+                                    while($data = mysqli_fetch_array($take)){                           
+                                ?>
+                                <?php
+                                    $batas = 10;
+                                    if(isset($_GET['halaman'])){
+                                        $halaman = (int)$_GET['halaman'];
+                                    }else{
+                                        $halaman = 1;
+                                    }
+                                    if($halaman > 1){
+                                        $halaman_awal = ($halaman * $batas) - $batas;
+                                    }else{
+                                        $halaman_awal = 0;
+                                    }
+                                    $data = mysqli_query($db, "SELECT * FROM buku");
+                                    $jumlah_data = mysqli_num_rows($data);
+                                    $total_halaman = ceil($jumlah_data/$batas);
+
+                                    $result = mysqli_query($db,"SELECT * FROM buku ORDER BY id_buku LIMIT $halaman_awal,$batas");
+                                    $no = $halaman_awal+1;
+                                    while ($data = mysqli_fetch_array($result)) {
                                 ?>
                                 <tr>
                                     <td><?= $data['id_buku'] ?> </td>
@@ -51,6 +70,9 @@ $take = mysqli_query($db,"SELECT * FROM buku");
                                         <a href="deletebuku.php?id_buku=<?= $data['id_buku'] ?>" class="btn btn-danger">hapus</a>
                                     </td>
                                 </tr>
+                                <?php
+                                }
+                                ?>
                                 <?php
                                 }
                                 ?>
