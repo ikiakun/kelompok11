799c9b2ed300981b1f927139b5372eab16210afc
ilhamalfa <kuro.nekogami22@gmail.com>
2022-09-29 15:11:12 +0700

    index

--- daftarbuku.php
+++ daftarbuku.php
@@ -17,7 +17,6 @@ $take = mysqli_query($db,"SELECT * FROM buku");
     
 </head>
 <body>
-<<<<<<< HEAD
     <!-- Navbar -->
     <nav class="navbar bg-white">
         <div class="container">
@@ -40,128 +39,137 @@ $take = mysqli_query($db,"SELECT * FROM buku");
             </ul>
         </div>
     </nav>
-=======
-                        <!-- Thor - Konten mysql -->
-                         <div class="container text-center mt-4">
-                            <table class="table table-striped table-hover mt-4">
-                            <thread>
-                                <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Penulis</th>
-                                <th scope="col">Tahun</th>
-                                <th scope="col">Judul</th>
-                                <th scope="col">Kota</th>
-                                <th scope="col">Penerbit</th>
-                                <th scope="col">Cover</th>
-                                <th scope="col">Sinopsis</th>
-                                <th scope="col">Stok</th>
-                                <th scope="col">Aksi</th>
-                                </tr>
-                            </thread>
-                            <tbody>
-                                <?php
-                                    while($data = mysqli_fetch_array($take)){                           
-                                ?>
-                                <?php
-                                    $batas = 10;
-                                    if(isset($_GET['halaman'])){
-                                        $halaman = (int)$_GET['halaman'];
-                                    }else{
-                                        $halaman = 1;
-                                    }
-                                    if($halaman > 1){
-                                        $halaman_awal = ($halaman * $batas) - $batas;
-                                    }else{
-                                        $halaman_awal = 0;
-                                    }
-                                    $data = mysqli_query($db, "SELECT * FROM buku");
-                                    $jumlah_data = mysqli_num_rows($data);
-                                    $total_halaman = ceil($jumlah_data/$batas);
 
-                                    $result = mysqli_query($db,"SELECT * FROM buku ORDER BY id_buku LIMIT $halaman_awal,$batas");
-                                    $no = $halaman_awal+1;
-                                    while ($data = mysqli_fetch_array($result)) {
-                                ?>
-                                <tr>
-                                    <td><?= $data['id_buku'] ?> </td>
-                                    <td><?= $data['penulis'] ?> </td>
-                                    <td><?= $data['tahun'] ?> </td>
-                                    <td><?= $data['judul'] ?> </td>
-                                    <td><?= $data['kota'] ?> </td>
-                                    <td><?= $data['penerbit'] ?> </td>
-                                    <td>
-                                        <img src="cover/<?= $data['cover'] ?>" alt="" class="img-thumbnail" style="width: 50px;">  
-                                    </td>
-                                    <td><?= $data['sinopsis'] ?> </td>
-                                    <td><?= $data['stok'] ?> </td>
-                                    <td colspan="2">
-                                        <a href="editbuku.php?id_buku=<?= $data['id_buku'] ?>" class ="btn btn-warning">edit</a>
-                                        <a href="deletebuku.php?id_buku=<?= $data['id_buku'] ?>" class="btn btn-danger">hapus</a>
-                                    </td>
-                                </tr>
-                                <?php
-                                }
-                                ?>
-                                <?php
-                                }
-                                ?>
-                            </tbody>
+    <div class="container">
+        <!-- card1 -->
+        <div class="row justify-content-center mt-3">
+            <div class="card border-0" style="background-color: bisque;">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-5">
+                            <h1 class="mt-5 fw-bold" style="color: darkslateblue;">Hi There!</h1>
+                            <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum rem nesciunt nemo, illo officia itaque provident sunt aliquid sapiente?</p>
                         </div>
-
->>>>>>> 2981727fdd4b0e13989d47404adf4923a7a7372a
-
-    <div class="row justify-content-center mt-3">
-        <div class="card border-0 w-75" style="background-color: bisque;">
-            <div class="card-body">
-                <div class="row"></div>
-                <div class="col-5">
-                    <h3 class="mt-3 fw-bold" style="color: darkslateblue;">Hi There!</h3>
-                    <p class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum rem nesciunt nemo, illo officia itaque provident sunt aliquid sapiente?</p>
-                </div>
-                <div class="col-3">
-                    <img src="assets/img/abstr.png" alt="" height="100px">
+                        <div class="col">
+                            <img src="assets/img/abstr.png" alt="" height="250px" class="mx-auto d-block">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-        <!-- <div class="container text-center mt-4">
-            <table class="table table-striped table-hover mt-4">
-            <thread>
-                <tr>
-                <th scope="col">No</th>
-                <th scope="col">Penulis</th>
-                <th scope="col">Tahun</th>
-                <th scope="col">Judul</th>
-                <th scope="col">Kota</th>
-                <th scope="col">Penerbit</th>
-                <th scope="col">Cover</th>
-                <th scope="col">Sinopsis</th>
-                <th scope="col">Stok</th>
-                </tr>
-            </thread>
-            <tbody>
-                <?php
+    
+        <h4 class="mt-3 fw-bold" style="color: darkslateblue;">Daftar Buku : </h4>
+        <div class="row justify-content-center">
+            <?php
                 while($data = mysqli_fetch_array($take)){
-                
-                ?>
-                <tr>
-                <td><?= $data['id_buku'] ?> </td>
-                <td><?= $data['penulis'] ?> </td>
-                <td><?= $data['tahun'] ?> </td>
-                <td><?= $data['judul'] ?> </td>
-                <td><?= $data['kota'] ?> </td>
-                <td><?= $data['penerbit'] ?> </td>
-                <td>
-                    <img src="cover/<?= $data['cover'] ?>" alt="" class="img-thumbnail" style="width: 50px;">  
-                </td>
-                <td><?= $data['sinopsis'] ?> </td>
-                <td><?= $data['stok'] ?> </td>
-                </tr>
-                <?php
+            ?>
+            <div class="col-2 mt-3">
+                <div class="card border-0">
+                    <img src="assets/img/books.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $data['judul'] ?></h5>
+                        <p class="card-text text-secondary">Stok : <?= $data['stok'] ?></p>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $data['id_buku'] ?>">
+                            Detail
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal<?= $data['id_buku'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel"><?= $data['judul'] ?></h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="assets/img/books.jpg" class="d-flex mx-auto" height="200px">
+                            <table class="table table-striped mt-3">
+                                <tbody>
+                                    <tr>
+                                        <td>No : </td>
+                                        <td><?= $data['id_buku'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>penulis : </td>
+                                        <td><?= $data['penulis'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>tahun : </td>
+                                        <td><?= $data['tahun'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>judul : </td>
+                                        <td><?= $data['judul'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>kota : </td>
+                                        <td><?= $data['kota'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>penerbit : </td>
+                                        <td><?= $data['penerbit'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>sinopsis : </td>
+                                        <td><?= $data['sinopsis'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>stok : </td>
+                                        <td><?= $data['stok'] ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
                 }
-                ?>
-            </tbody>
-        </div> -->
+            ?>
+        </div>
+    </div>
+    
+    <!-- <div class="container text-center mt-4">
+        <table class="table table-striped table-hover mt-4">
+        <thread>
+            <tr>
+            <th scope="col">No</th>
+            <th scope="col">Penulis</th>
+            <th scope="col">Tahun</th>
+            <th scope="col">Judul</th>
+            <th scope="col">Kota</th>
+            <th scope="col">Penerbit</th>
+            <th scope="col">Cover</th>
+            <th scope="col">Sinopsis</th>
+            <th scope="col">Stok</th>
+            </tr>
+        </thread>
+        <tbody>
+            <?php
+            while($data = mysqli_fetch_array($take)){
+            
+            ?>
+            <tr>
+            <td><?= $data['id_buku'] ?> </td>
+            <td><?= $data['penulis'] ?> </td>
+            <td><?= $data['tahun'] ?> </td>
+            <td><?= $data['judul'] ?> </td>
+            <td><?= $data['kota'] ?> </td>
+            <td><?= $data['penerbit'] ?> </td>
+            <td>
+                <img src="cover/<?= $data['cover'] ?>" alt="" class="img-thumbnail" style="width: 50px;">  
+            </td>
+            <td><?= $data['sinopsis'] ?> </td>
+            <td><?= $data['stok'] ?> </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </div> -->
 
     <!-- Bootstrap JS -->
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
