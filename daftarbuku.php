c3ad7501d8999e08e41a5ed2d5505a667bc54b32
ikiakun <ikiakun@icloud.com>
2022-09-29 13:24:07 +0700

    add

--- daftarbuku.php
+++ daftarbuku.php
@@ -1,3 +1,8 @@
+<?php
+include ('config.php');
+$take = mysqli_query($db,"SELECT * FROM buku");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +13,46 @@
 </head>
 <body>
     
+                         <div class="container text-center mt-4">
+                            <table class="table mt-4">
+                            <thread>
+                                <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Penulis</th>
+                                <th scope="col">Tahun</th>
+                                <th scope="col">Judul</th>
+                                <th scope="col">Kota</th>
+                                <th scope="col">Penerbit</th>
+                                <th scope="col">Cover</th>
+                                <th scope="col">Sinopsis</th>
+                                <th scope="col">Stok</th>
+                                </tr>
+                            </thread>
+                            <tbody>
+                                <?php
+                                while($data = mysqli_fetch_array($take)){
+                                
+                                ?>
+                                <tr>
+                                <td><?= $data['no'] ?> </td>
+                                <td><?= $data['penulis'] ?> </td>
+                                <td><?= $data['tahun'] ?> </td>
+                                <td><?= $data['judul'] ?> </td>
+                                <td><?= $data['kota'] ?> </td>
+                                <td><?= $data['Penerbit'] ?> </td>
+                                <td>
+                                    <img src="cover/<?= $data['cover'] ?>" alt="" class="img-thumbnail" style="width: 50px;">  
+                                </td>
+                                <td><?= $data['Sinopsis'] ?> </td>
+                                <td><?= $data['Stok'] ?> </td>
+                                </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </div>
+
+
+
 </body>
 </html>
